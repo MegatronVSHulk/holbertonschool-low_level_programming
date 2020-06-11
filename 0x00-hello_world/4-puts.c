0cd270e52ce0538295562197a83a5b46a049588d
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 20:16:21 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,12 +1,12 @@
+#include <stdio.h>
 /**
-* Main - function puts the string to the console
+* main - function puts the string to the console
 *
 * Description: the function main writes the string into the terminal
 *
 * Return: returns 0
 *
 */
-#include <stdio.h>
 int main(void) 
 {
 puts("\"Programming is like building a multilingual puzzle");
