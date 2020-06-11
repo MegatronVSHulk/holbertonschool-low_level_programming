8311b902ed7fc46a8146efdb31d9363a8ce37e8a
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 17:50:11 -0500

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
-
-/* betty style doc for function main goes there */
+/**
+ * Main - Primary function being executed
+(*
+ * Description: Prints entire alphabet twice, lowercase then uppercase
+**/
 int main(void)
 {
   char CASEBIG;
