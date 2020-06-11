5c9f18c3f17f56eaf35c88ea7ee11f7d496941f1
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 19:52:54 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
-/* first program using puts */
-/* Main - puts text on screen */
+/* Main function puts the string to the console
+* This is a very short command
+* Then is returns the value of 0 */
 int main() {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
