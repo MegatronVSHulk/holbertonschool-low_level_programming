61f29db0dc8445f7edeac48f90a2373b5af86637
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 17:48:33 -0500

    Update 10-print_comb2.c

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
-/* COMMENT */
+/** Main - Primary function being executed
+(*
+ * Description: Integer variable COUNT is declared. COUNT is
+ * given initial value of 00. We then start with a For Loop that 
+ * continues as long as COUNT is less than or equal to 99. There
+ * is an IF/ELSE statement that helps determine if another comma 
+ * and space are needed, or if the program is complete and ready
+ * to create a new line upon ending
+**/
 int main(void)
 {
   int COUNT;
