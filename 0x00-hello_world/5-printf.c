9cc17ed6ffcfea1a5c56073901e2629ee88c22d8
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 20:30:34 -0500

    Update 5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,5 +1,13 @@
 #include<stdio.h>
-int main() {
+/**
+ * main - function puts the string to the console
+(* a blank line
+ * Description: the function main writes the string into the terminal
+(* a blank line
+ * Return: returns 0
+ *
+ */
+int main(void) {
 printf("with proper grammar, but the outcome is a piece of art,\n");
-return 0;
+return (0);
 }
