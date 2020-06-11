3ffda68b67b40af2e033a671b77ae466ae8e3dfb
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 19:59:59 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
-/* Main function puts the string to the console */
+/* 
+* Main - function puts the string to the console
+*
+* Description: pushes string to the terminal console
+* 
+*/
 int main() 
 {
   puts("\"Programming is like building a multilingual puzzle");
