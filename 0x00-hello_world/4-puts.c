2a1e8a5aca882c92c458f0fe1bc6a9edcedcb52c
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 19:54:27 -0500

    Update 4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,6 +1,8 @@
 /* Main function puts the string to the console
 * This is a very short command
-* Then is returns the value of 0 */
+*
+* Then is returns the value of 0 for the terminal and shell
+*/
 int main(void) {
   puts("\"Programming is like building a multilingual puzzle");
   return (0);
