a3b925585ef2c6d84fda110960049e8d4f694102
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 20:40:44 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,5 +1,18 @@
 #include<stdio.h>
-int main() {
+/**
+ * main - function puts the string to the console
+ * @intSize: First operand
+ * @charSize: Second operand
+ * @LongIntSize: Third operand
+ * @LLIS: Forth final operand
+(* a blank line
+ * Description: the function main writes the string into the terminal
+(* a blank line
+ * Return: returns 0
+ *
+ */
+int main(void)
+{
   int intSize;
   char charSize;
   long int LongIntSize;
@@ -10,5 +23,5 @@ int main() {
   printf("Size of a long int: %lx byte(s)\n", sizeof(LongIntSize));
   printf("Size of a long long int: %lx byte(s)\n", sizeof(LLIS));
   printf("Size of a float: %lx byte(s)\n", sizeof(floatSize));
-  return 0;
+  return (0);
 }
