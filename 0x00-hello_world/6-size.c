b670094b1b5cb901ddeae65ccb0669218575c084
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-10 19:07:00 -0500

    Update 6-size.c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -5,10 +5,10 @@ int main() {
   long int LongIntSize;
   long long int LLIS;
   float floatSize;
-  printf("Size of char: %ld byte(s)\n", sizeof(charSize));
-  printf("Size of an int: %ld byte(s)\n", sizeof(intSize));
-  printf("Size of a long int: %ld byte(s)\n", sizeof(LongIntSize));
-  printf("Size of a long long int: %ld byte(s)\n", sizeof(LLIS));
-  printf("Size of a float: %ld byte(s)\n", sizeof(floatSize));
-  return (0);
+  printf("Size of char: %lx byte(s)\n", sizeof(charSize));
+  printf("Size of an int: %lx byte(s)\n", sizeof(intSize));
+  printf("Size of a long int: %lx byte(s)\n", sizeof(LongIntSize));
+  printf("Size of a long long int: %lx byte(s)\n", sizeof(LLIS));
+  printf("Size of a float: %lx byte(s)\n", sizeof(floatSize));
+  return 0;
 }
