c7c4511fe13f7909a9bb1f483be57ae3f8c58749
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:58:41 -0500

    Update 4-print_alphabt.c

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,19 +1,19 @@
 #include <stdio.h>
-/**
- * Main - Primary function being executed
-(*
- * Description: Prints entire alphabet, lowercase, excludes
- * the letter 'e' and 'q' with an IF statement existing within
- * the FOR statement.
- */
+/* Main - Primary function being executed
+*
+* Description: Prints entire alphabet, lowercase, excludes
+* the letter 'e' and 'q' with an IF statement existing within
+* the FOR statement.
+* Return: 0
+*/
 int main(void)
 {
-  char letter;
-  for (letter = 'a'; letter <= 'z'; letter++){
-    if(letter != 'e' && letter != 'q')
-    {
-      putchar(letter);
-    }
-  }
-  return (0);
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++){
+if(letter != 'e' && letter != 'q')
+{
+putchar(letter);
+}
+}
+return (0);
 }
