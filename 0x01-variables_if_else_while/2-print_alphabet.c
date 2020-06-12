5b602b7574ad2efa5af2b7d865d765325c4b5299
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:53:04 -0500

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,17 +1,16 @@
 #include <stdio.h>
-/*
- * Main - Primary function being executed
- * a blank line
- * Description: Prints entire alphabet, lowercase
- * Return: returns 0
- */
+/* Main - Primary function being executed
+* a blank line
+* Description: Prints entire alphabet, lowercase
+* Return: returns 0
+*/
 int main(void)
 {
-  char letter;
-  for (letter == 'a'; letter <= 'z'; letter++)
-  {
-    putchar(letter);
-  }
-  putchar('\n');
-  return (0);
+char letter;
+for (letter == 'a'; letter <= 'z'; letter++)
+{
+putchar(letter);
+}
+putchar('\n');
+return (0);
 }
