f5ec88d759b49e2cdc7afdc7f8410bc844110ee1
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-12 14:32:16 -0500

    Update 9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -1,19 +1,21 @@
 #include <stdio.h>
-/*Main - Primary function being executed
-*Description: @COUNT initiates at 0 and is used to count up to 9.
-*Return: 0
+/*
+* Main - Primary function being executed
+* Description: @COUNT initiates at 0 and is used to count up to 9.
+* Return: 0
 **/
 int main(void)
 {
 int COUNT;
-for(COUNT = 0; COUNT <= 9; COUNT++)
+for (COUNT = 0; COUNT <= 9; COUNT++)
 {
 putchar('0' + COUNT);
-if(COUNT != 9)
+if (COUNT != 9)
 {
 putchar(',');
 putchar(' ');
-} else
+}
+else
 {
 putchar('\n');
 }
