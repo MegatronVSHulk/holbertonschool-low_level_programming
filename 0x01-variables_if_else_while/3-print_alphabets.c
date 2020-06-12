b088881f86639305d15b29d3802ed3815c4fd77f
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:54:39 -0500

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,19 +1,20 @@
 #include <stdio.h>
-/**
- * Main - Primary function being executed
-(*
- * Description: Prints entire alphabet twice, lowercase then uppercase
+/* Main - Primary function being executed
+*
+* Description: Prints entire alphabet twice, lowercase then uppercase
 **/
 int main(void)
 {
-  char CASEBIG;
-  char CASESMALL;
-  for(CASESMALL = 'a'; CASESMALL <= 'z'; CASESMALL++){
-    putchar(CASESMALL);
-  }
-  for(CASEBIG = 'A'; CASEBIG <= 'Z'; CASEBIG++){
-    putchar(CASEBIG);
-  }
-  putchar('\n');
-  return (0);
+char CASEBIG;
+char CASESMALL;
+for(CASESMALL = 'a'; CASESMALL <= 'z'; CASESMALL++)
+{
+putchar(CASESMALL);
+}
+for(CASEBIG = 'A'; CASEBIG <= 'Z'; CASEBIG++)
+{
+putchar(CASEBIG);
+}
+putchar('\n');
+return (0);
 }
