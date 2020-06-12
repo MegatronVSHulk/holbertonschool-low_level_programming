5138c28b6385e701e4c544c3ca588b3633756fa5
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:31:05 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -1,6 +1,6 @@
-#include <stdlib.h>
 #include <time.h>
 #include <stdio.h>
+#include <stdlib.h>
 /* Main - The primary function being carried out
 (* a blank line
  * Description: The integer variable 'n' which is
