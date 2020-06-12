8a8ef8a1a28db0587d7200f8553cb1872ed7acb0
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:59:50 -0500

    Update 5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
-/**
- * Main - Primary function being executed
-(*
- * Description: Prints the number 0-9, ascending order
+/* Main - Primary function being executed
+*
+* Description: Prints the number 0-9, ascending order
+* Return: 0
 **/
 int main(void)
 {
-	int NUMBER;
-	for(NUMBER = 0; NUMBER < 10; NUMBER++){
-	  printf("%d", NUMBER);
-	}
-	putchar('\n');
-	return (0);
-} 
+int NUMBER;
+for(NUMBER = 0; NUMBER < 10; NUMBER++){
+printf("%d", NUMBER);
+}
+putchar('\n');
+return (0);
+}
