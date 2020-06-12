81016ef5307926073e2021b8faef899bb0861ddf
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 21:29:45 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -2,30 +2,31 @@
 #include <stdio.h>
 #include <stdlib.h>
 /* Main - The primary function being carried out
-(* a blank line
- * Description: The integer variable 'n' which is
- * stapled to integer variable RESULT. RESULT is used
- * in coordination with the 3 IF/THEN statements that 
- * weight its value against 0, 5, and 6.
+* a blank line
+* Description: The integer variable 'n' which is
+* stapled to integer variable RESULT. RESULT is used
+* in coordination with the 3 IF/THEN statements that 
+* weight its value against 0, 5, and 6.
+* Return: 0
 **/
 int main(void)
 {
-  int n;
-  int RESULT;
-  srand(time(0));
-  n = rand() - RAND_MAX / 2;
-  RESULT = n % 10;
-  if (RESULT > 5)
-  {
-    printf("The last digit of %d is %d and is greater than 5\n", n, RESULT);
-  }
-  if (RESULT == 0)
-  {
-    printf("The last digit of %d is %d and is 0\n", n, RESULT);
-  }
-  if (RESULT < 6 && RESULT != 0)
-  {
-    printf("The last digit of %d is %d and is less than 6 and not 0\n", n, RESULT);
-  }
-  return (0);
+int n;
+int RESULT;
+srand(time(0));
+n = rand() - RAND_MAX / 2;
+RESULT = n % 10;
+if (RESULT > 5)
+{
+printf("The last digit of %d is %d and is greater than 5\n", n, RESULT);
+}
+if (RESULT == 0)
+{
+printf("The last digit of %d is %d and is 0\n", n, RESULT);
+}
+if (RESULT < 6 && RESULT != 0)
+{
+printf("The last digit of %d is %d and is less than 6 and not 0\n", n, RESULT);
+}
+return (0);
 }
