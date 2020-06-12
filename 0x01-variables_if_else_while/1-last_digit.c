5e8493e2302ba4ba8762e476eb85f7426454078a
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:23:34 -0500

    Update 1-last_digit.c

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -13,41 +13,21 @@ int main(void)
   int n;
   srand(time(0));
   n = rand() - RAND_MAX / 2;
-  /* Print first string to terminal */
-  printf("Last digit of ");
-  /* We create new variable RESULT that
-   * grabs last digit of integer. We then
-   * print the result to terminal, followed
-   * by the string " is " and the RESULT.
-   */
   int RESULT;
   int pRESULT;
   RESULT = n % 10;
-  printf("%d", n);
-  printf(" is ");
-  printf("%d", RESULT);
-  /* Now we introduce IF/THEN/ELSE statements
-   * into the code that compare the value of
-   * RESULT weighed against 0, 5, and 6. True
-   * statements will print the cooresponding 
-   * string.
-   */
-  pRESULT = (RESULT + '0');
-  if (pRESULT > 5)
+  printf("The last digit of %d is %d ", n, RESULT);
+  if (RESULT > 5)
   {
-    /* If last digit, RESULT, is greater than 5 */
-    printf(" and is greater than 5");
+    printf(" and is greater than 5\n");
   }
-  if (pRESULT = 0)
+  if (RESULT = 0)
   {
-    /* If last digit, RESULT, is zero */
-    printf(" and is 0");
+    printf(" and is 0\n");
   }
-  if (pRESULT < 6 && RESULT != 0)
+  if (RESULT < 6 && RESULT != 0)
   {
-    /* If last digit, RESULT, is less than 6 and not 0 */
-    printf(" and is less than 6 and not 0");
+    printf(" and is less than 6 and not 0\n");
   }
-  putchar('\n');
   return (0);
 }
