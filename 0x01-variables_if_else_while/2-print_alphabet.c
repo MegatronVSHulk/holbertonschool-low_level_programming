23f01bd772658cb8bb33242544e23b5511cd2aed
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 20:08:45 -0500

    Update 2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * Main - Primary function being executed
-(* a blank line
+ * a blank line
  * Description: Prints entire alphabet, lowercase
  * Return: returns 0
- *
  */
+
 int main(void)
 {
   char letter;
