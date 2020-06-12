4590c706638d4e857b89614875a330b9597ef19d
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-11 21:00:43 -0500

    Update 6-print_numberz.c

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -1,17 +1,17 @@
 #include <stdio.h>
-/**
- * Main - Primary function being executed
- * integer variable number is declared then put
- * through the For loop with an ++ increment.
- * We then use putchar (VAR + '0') which shifts 
- * from the ASCII output to the literal output 
- * of the integer. It is then printed on console
+/* Main - Primary function being executed
+* integer variable number is declared then put
+* through the For loop with an ++ increment.
+* We then use putchar (VAR + '0') which shifts 
+* from the ASCII output to the literal output 
+* of the integer. It is then printed on console
 **/
 int main(void)
 {
-  int number;
-  for(number = 0; number < 10; number++){
-    putchar(number + '0');
-  }
-  return (0);
+int number;
+for(number = 0; number < 10; number++)
+{
+putchar(number + '0');
+}
+return (0);
 }
