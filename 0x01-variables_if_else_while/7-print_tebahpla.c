2a30bf7d800dcafc38600131ece3244c01d56b51
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-12 20:19:14 -0500

    Update 7-print_tebahpla.c

--- 0x01-variables_if_else_while/7-print_tebahpla.c
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -1,9 +1,11 @@
 #include <stdio.h>
-/*
-* Main - Primary function being executed
-*
+/**
+* main - Primary function being executed
+* a blank line
 * Description: Prints entire alphabet, lowercase, in reverse
-**/
+* a blank line
+* Return: returns 0
+*/
 int main(void)
 {
 char LETTER;
