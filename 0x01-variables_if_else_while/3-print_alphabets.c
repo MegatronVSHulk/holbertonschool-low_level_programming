341ee9a497136a059211bfd98309361c6ed0ebe7
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-12 20:03:12 -0500

    Update 3-print_alphabets.c

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 /**
-* Main - Primary function being executed
+* main - Primary function being executed
 * a blank line
 * Description: Prints entire alphabet twice, lowercase then uppercase
 * a blank line
