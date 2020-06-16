4cc013a708fc74898420825656767c6e301f4d43
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:46:36 -0500

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -7,21 +7,19 @@
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
-*/ 
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
+*/
 int _isalpha(int c)
 {
-  int val;
-  if (c > 64 && c < 91)
-    { 
-      val = 1;
-    } else if (c > 96 && < 123){
-    val = 1;
-  } else {
-    val = 0;
-  }
-  return (val);
+int val;
+if (c > 64 && c < 91)
+{ 
+val = 1;
+} else if (c > 96 && < 123)
+{
+val = 1;
+} else
+{
+val = 0;
+}
+return (val);
 }
