1d5783d78c786c8942d21aa22e3f34c756c44724
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:48:50 -0500

    Update 6-abs.c

--- 0x02-functions_nested_loops/6-abs.c
+++ 0x02-functions_nested_loops/6-abs.c
@@ -7,19 +7,16 @@
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
-*/ 
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
+*/
 int _abs (int n)
 {
-  int val;
-  if (n < 0 && n != 0)
-    {
-      val = (n * -1);
-    }  else {
-      val = n;
-    }
-  return (val);
+int val;
+if (n < 0 && n != 0)
+{
+val = (n * -1);
+} else
+{
+val = n;
+}
+return (val);
 }
