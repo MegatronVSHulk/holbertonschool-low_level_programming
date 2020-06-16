24d7abb0313a4b3609db3abde74738bec259b06f
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:47:49 -0500

    Update 5-sign.c

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -8,22 +8,18 @@
 * a blank line
 * Return: returns 0
 */ 
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
 int _print_sign(int n)
 {
-  int val;
-  if (n > 0 && n != 0)
-    {
-      val = '1';
-    } else if (n < 0 && n != 0)
-    {
-      val = '-1';
-    } else
-    {
-      val = 0;
-    }  
-  return (val);
+int val;
+if (n > 0 && n != 0)
+{
+val = '1';
+} else if (n < 0 && n != 0)
+{
+val = '-1';
+} else
+{
+val = 0;
+}
+return (val);
 }
