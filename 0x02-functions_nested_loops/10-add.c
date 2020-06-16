65a6ff00609c2d6a6800bbbc3c325b42ec746350
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 17:28:20 -0500

    Create 10-add.c

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+* add - The primary function being carried out
+* @a: the first character in formula
+* @b: the second character in formula
+* Description: Parses 'a' and 'b' and returns 'val'
+* a blank line
+* Return: returns @val
+*/
+int add(int a, int b)
+{
+int val;
+val = (a + b);
+_putchar(val + '0');
+return (val);
+}
