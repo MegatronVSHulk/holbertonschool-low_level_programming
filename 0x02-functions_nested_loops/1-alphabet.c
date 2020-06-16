e68a9000e952fb4723cd6f132791d465c306e8ad
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:41:59 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -7,24 +7,20 @@
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
-*/ 
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
+*/
 void print_alphabet(void);
 {
-  char letter;
-  letter = 'a';
-  while (letter < 123)
-    {
-      _putchar(letter);
-      letter++;
-    }
-  _putchar('\n');
-  return (0);
+char letter;
+letter = 'a';
+while (letter < 123)
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+return (0);
 }
 int main(void);
 {
-  print_alphabet();
+print_alphabet();
 }
