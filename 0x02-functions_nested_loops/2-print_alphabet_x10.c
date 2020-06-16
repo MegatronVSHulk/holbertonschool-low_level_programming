b2e72ed71efe09b7f8b1dc11bb80b1ffd36d4103
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 15:12:36 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -2,8 +2,6 @@
 /**
 * print_alphabet_x10 - The primary function being carried out
 * a blank line
-* _putchar - Function for printing characters
-* a blank line
 * Description: The integer variable 'n' which is
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
@@ -14,7 +12,7 @@ void print_alphabet_x10(void);
 char letter;
 int count;
 letter = 'a';
-count = 0; 
+count = 0;
 while (count < 10)
 {
 while (letter < 123)
