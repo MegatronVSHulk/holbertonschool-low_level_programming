802b45e9c2158aa127b7f0568d3d522926b54771
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 16:10:06 -0500

    Update 4-isalpha.c

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,13 +1,12 @@
 #include "holberton.h"
 /**
-* print_alphabet - The primary function being carried out
-* a blank line
-* _putchar - Function for printing characters
+* _isalpha - The primary function being carried out
+* @c: integer or char value
 * a blank line
 * Description: The integer variable 'n' which is
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
-* Return: returns 0
+* Return: returns @val
 */
 int _isalpha(int c)
 {
