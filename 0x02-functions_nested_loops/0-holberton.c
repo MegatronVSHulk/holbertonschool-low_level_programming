1cf27316472aeb7c54a770c919cbcde4f9507ec3
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 09:32:26 -0500

    Update 0-holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,17 +1,11 @@
 /**
 * main - The primary function being carried out
 * a blank line
-* _putchar - Function for printing characters
-* a blank line
 * Description: The integer variable 'n' which is
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
 */
-int _putchar(char c)
-{
-return (write(1, &c, 1));
-}
 int main(void)
 {
 char word[] = "Holberton";
