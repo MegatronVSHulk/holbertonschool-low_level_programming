aa8f4a62d0c956188bd02a9e71d28bc7836d369f
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:45:38 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -8,32 +8,28 @@
 * a blank line
 * Return: returns 0
 */ 
-int _putchar(char c)
-{
-  return (write(1, &c, 1));
-}
 int _islower(int c)
 {
-  int val;
-  if (c > 96 && c < 123)
-    { 
-      val = 1;
-    } else if (c < 96 || c > 123)
-    {
-      val = 0;
-    }
-  return (val);
+int val;
+if (c > 96 && c < 123)
+{ 
+val = 1;
+} else if (c < 96 || c > 123)
+{
+val = 0;
+}
+return (val);
 }
 int main(void)
 {
-  int r;
-  r = _islower('o');
-  if (r != 1){
-    _putchar('L');
-  }
-  else
-  {
-    _putchar('U');
-  }
-  return (0);
+int r;
+r = _islower('o');
+if (r != 1){
+_putchar('L');
+}
+else
+{
+_putchar('U');
+}
+return (0);
 }
