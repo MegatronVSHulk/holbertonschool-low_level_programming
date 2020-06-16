372d3dff9d4f049a67876de45f5eca7b3e88664c
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 09:03:32 -0500

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,8 +1,6 @@
 /**
 * print_alphabet - The primary function being carried out
 * a blank line
-* _putchar - Function for printing characters
-* a blank line
 * Description: The integer variable 'n' which is
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
