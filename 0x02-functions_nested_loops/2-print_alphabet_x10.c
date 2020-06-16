f8cf8bb2e76ac74aea51766e1efa92275e495cd1
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 08:44:28 -0500

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -7,23 +7,23 @@
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
-*/ 
+*/
 void print_alphabet_x10(void);
 {
-  char letter;
-  int count;
-  letter = 'a';
-  count = 0; 
-  while (count < 10)
-    {
-      while (letter < 123)
-	{
-	  _putchar(letter);
-	  letter++;
-	}
-      _putchar('\n');
-      count++;
-      letter = 'a';
-    }
-  return (0);
+char letter;
+int count;
+letter = 'a';
+count = 0; 
+while (count < 10)
+{
+while (letter < 123)
+{
+_putchar(letter);
+letter++;
+}
+_putchar('\n');
+count++;
+letter = 'a';
+}
+return (0);
 }
