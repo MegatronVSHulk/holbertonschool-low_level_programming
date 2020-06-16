f5bfa2825b2b368bafbad19da3f11eb69c71ed66
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 09:06:59 -0500

    Update 3-islower.c

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,13 +1,11 @@
 /**
 * print_alphabet - The primary function being carried out
 * a blank line
-* _putchar - Function for printing characters
-* a blank line
 * Description: The integer variable 'n' which is
 * stapled to integer variable RESULT. RESULT is printed.
 * a blank line
 * Return: returns 0
-*/ 
+*/
 int _islower(int c)
 {
 int val;
@@ -19,17 +17,4 @@ val = 1;
 val = 0;
 }
 return (val);
-}
-int main(void)
-{
-int r;
-r = _islower('o');
-if (r != 1){
-_putchar('L');
-}
-else
-{
-_putchar('U');
-}
-return (0);
 }
