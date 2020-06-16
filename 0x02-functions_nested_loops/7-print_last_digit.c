c54c1cdbea0f29ae29f34aefcfb3059fb31a6506
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 15:16:23 -0500

    Update 7-print_last_digit.c

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,13 +1,13 @@
 #include "holberton.h"
 /**
 * print_last_digit - The primary function being carried out
-* a blank line
+* @n: the integer that is parsed for last number
 * Description: The integer variable 'val' which is
 * stapled to integer variable n. last digit is printed.
 * a blank line
 * Returns: returns val
 */
-int print_last_digit (int n)
+int print_last_digit(int n)
 {
 int val;
 val = (n % 10);
