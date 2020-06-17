d9c602e8f1a05a238310f5f1b631d3f3336ad40b
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-16 19:18:16 -0500

    Update 11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -3,9 +3,6 @@
 /**
 * print_to_98 - The primary function being carried out
 * @n: is parsed and prints difference from 'n' to 98
-* @isPositive: determine if positive
-* @difference: determine difference between 2 inputs
-* @actual: actual difference weighed
 * a blank line
 * Description: Function takes input, print input - 98
 * a blank line
@@ -19,20 +16,18 @@ int actual;
 printf("%d, ", n);
 if (n > 98)
 {
-difference = n;
-while (difference > 98)
+while (n > 98)
 {
-printf("%d, ", difference);
-difference--;
+printf("%d, ", n);
+n--;
 }
 }
 if (n > 0 && n < 98)
-difference = n;
 {
-while (difference < 98)
+while (n < 98)
 {
-printf("%d, ", difference);
-difference++;
+printf("%d, ", n);
+n++;
 }
 }
 if (n < 0)
