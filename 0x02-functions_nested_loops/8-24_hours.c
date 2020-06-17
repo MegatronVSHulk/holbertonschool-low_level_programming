1c8b63d24715a20ca8d19eb5eb5808c67bac8647
MegatronVSHulk <chrisdebon@email.com>
2020-06-17 19:49:35 +0000

    Reupload

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,32 +1,39 @@
 #include "holberton.h"
 /**
-* jack_bauer - The primary function being carried out
+* jack_bauer - Primary function being executed
 * a blank line
-* Description: The integer variable 'n' which is
-* stapled to integer variable RESULT. RESULT is printed.
+* Description: Prints military time up to 23:59 via putchar
 * a blank line
 * Return: returns 0
-*/ 
+*/
 void jack_bauer(void)
 {
-int m, h, d;
-h = 0;
-while (h < 24)
-{
-m = 0;
-while (m < 60)
-{
-d = h / 10;
-_putchar('0' + d);
-d = h % 10;
-_putchar('0' + d);
-_putchar(':');
-d = m / 10;
-_putchar('0' + d);
-d = m % 10;
-_putchar('\n');
-m++;
-}
-h++
-}
+  int h1, h2, m1, m2;
+
+  for (h1 = 0; h1 <= 2; h1++)
+    {
+      for (h2 = 0; h2 <= 9; h2++)
+	{
+	  if (h1 == 2 && h2 == 4)
+	    {
+	      break;
+	    } else
+	    {
+	      
+	      for (m1 = 0; m1 < 6; m1++)
+		{
+		  for (m2 = 0; m2 <= 9; m2++)
+		    {
+		    
+		      _putchar(h1 + '0');
+		      _putchar(h2 + '0');
+		      _putchar(':');
+		      _putchar(m1 + '0');
+		      _putchar(m2 + '0');
+		      _putchar('\n');
+		    }
+		}
+	    }
+	}
+    }
 }
