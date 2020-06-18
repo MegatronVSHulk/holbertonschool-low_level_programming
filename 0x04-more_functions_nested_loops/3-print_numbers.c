38206d856c37f410e8162a64b1c3920a73c74d8e
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-18 18:21:41 -0500

    Update 3-print_numbers.c

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,21 +1,19 @@
 #include "holberton.h"
 /**
-* _mul - The primary function being carried out
-* @num: Character array
-* @count: Counter integer
+* print_numbers - The primary function being carried out
 * a blank line
 * Return: returns 0
 */
 void print_numbers(void)
 {
-  char num[] = "0123456789";
-  int count;
-  count = 0;
-  while (count < 10)
-    {
-      _putchar(num[count]);
-      count++;
-    }
-  _putchar('$');
-  _putchar('\n');
+char num[] = "0123456789";
+int count;
+count = 0;
+while (count < 10)
+{
+_putchar(num[count]);
+count++;
+}
+_putchar('$');
+_putchar('\n');
 }
