b71365eb3c65d97e4fce254cef089d969ab8b320
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-18 18:38:35 -0500

    Update 10-print_triangle.c

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -7,37 +7,37 @@
 */
 void print_triangle(int size)
 {
-  int height;
-  int length;
-  int noSpaces;
-  int noBlocks;
-  length = 1;
-  noSpaces = (size - length);
-  height = size;
-  if (size > 0)
-    {
-      while (height > 0)
-	{
-	  while (noSpaces > 0)
-	    {
-	      _putchar(' ');
-	      noSpaces--;
-	    }
-	  while (noBlocks > 0)
-	    {
-	      _putchar('#');
-	      noBlocks--;
-	    }
-	  noSpaces = (size - length);
-	  noBlocks = length;
-	  length++;
-	  height--;
-	  _putchar('\n');
-	}
-      _putchar('\n');
-    }
-  else
-    {
-      _putchar('\n');
-    }
+int height;
+int length;
+int noSpaces;
+int noBlocks;
+length = 1;
+noSpaces = (size - length);
+height = size;
+if (size > 0)
+{
+while (height > 0)
+{
+while (noSpaces > 0)
+{
+_putchar(' ');
+noSpaces--;
+}
+while (noBlocks > 0)
+{
+_putchar('#');
+noBlocks--;
+}
+noSpaces = (size - length);
+noBlocks = length;
+length++;
+height--;
+_putchar('\n');
+}
+_putchar('\n');
+}
+else
+{
+_putchar('\n');
+}
 }
