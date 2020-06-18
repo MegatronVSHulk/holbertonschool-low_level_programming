b380ce45eea733c652a8cfdd0435717fed6ed2d0
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-18 18:25:53 -0500

    Update 5-more_numbers.c

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -1,37 +1,35 @@
 #include "holberton.h"
 /**
 * more_numbers - The primary function being carried out
-* @num: Character array
-* @count: Counter integer
 * a blank line
 * Return: returns 0
 */
 void more_numbers(void)
 {
-  int num;
-  int num1;
-  int num2;
-  int mainCount;
-  mainCount = 0;
-  num = 0;
-  while (mainCount < 10)
-    {
-      while (num < 15)
-	{
-	  num1 = ((num % 10) + '0');
-	  num2 = ((num / 10) + '0');
-	  if (num >= 9)
-	    {
-	      _putchar(num2);
-	    }  
-	  _putchar(num1);
-	  num++;  
-	}
-      if (num >= 14)
-	{
-	  _putchar('\n');
-	  mainCount++;
-	  num = 0;
-	}
-    }
+int num;
+int num1;
+int num2;
+int mainCount;
+mainCount = 0;
+num = 0;
+while (mainCount < 10)
+{
+while (num < 15)
+{
+num1 = ((num % 10) + '0');
+num2 = ((num / 10) + '0');
+if (num >= 9)
+{
+_putchar(num2);
+}  
+_putchar(num1);
+num++;  
+}
+if (num >= 14)
+{
+_putchar('\n');
+mainCount++;
+num = 0;
+}
+}
 }
