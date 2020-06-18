008ac331337083fb7baa072b416064c035039e3c
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-18 18:17:26 -0500

    Update 0-isupper.c

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -7,15 +7,15 @@
 */
 int _isupper(int c)
 {
-  int val;
-  val = c;
-  if (val <= 90 && val >= 60)
-    {
-      val = 1;
-    }
-  else
-    {
-      val = 0;
-    }
-  return (val);
+int val;
+val = c;
+if (val <= 90 && val >= 60)
+{
+val = 1;
+}
+else
+{
+val = 0;
+}
+return (val);
 }
