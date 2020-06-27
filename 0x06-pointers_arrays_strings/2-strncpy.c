5ba18637839deb65ad39e10ed7ff7e5df7015e60
Chris <59417271+MegatronVSHulk@users.noreply.github.com>
2020-06-27 00:02:38 -0500

    Update 2-strncpy.c

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -1,4 +1,4 @@
-#include "holberton.h"
+ #include "holberton.h"
 /**
 * _strncpy - combines two strings with pointers
 * @dest: the destination of combined string
@@ -9,18 +9,16 @@
 */
 char *_strncpy(char *dest, char *src, int n)
 {
-  int i = 0;
-  int o = 0;
-  int r = 0;
-  while (dest[i] != '\0')
-    {
-      i++; // gets the length of dest array
-    }
-  while (o < n) // while o, which equals zero, is under n, our byte limit
-    {
-      src[o] = dest[r]; // makes src array = dest array value, both start from 0
-      r++; // r and o increment by 1
-      o++; // increments by 1
-    }
-  return (src); // returns the src array as the result
+int o = 0;
+while ((o < n) && (o != '\0'))
+{
+dest[o] = src[o];
+o++;
+}
+while (o < n)
+{
+dest[o] = '\0';
+o++;
+}
+return (dest);
 }
